73106bc2db79f89ceff5c2de4adb4b834de170c9
MichaelDimmitt <michaelgdimmitt@gmail.com>
2016-12-28 19:35:47 -0500

    cleaned up code with ryan

--- Rakefile.rb
+++ Rakefile.rb
@@ -49,48 +49,10 @@ task :create_sub_repos, [:master_repo_dir, :github_user] do |t, args|
 require 'io/console'
 require 'json'
 
-def folderName() puts "Please enter folder name: "; folder = STDIN.gets; end
-def accountName(str) puts "Please enter #{str}: "; username = STDIN.gets; end
-def accountPassword(str) puts "Please enter #{str}: "; password = STDIN.noecho(&:gets); end
+def folderName() puts "Please enter folder name: "; folder = STDIN.gets; end #called specifically in task
+def accountName(str) puts "Please enter #{str}: "; username = STDIN.gets; end #inputsToUser
+def accountPassword(str) puts "Please enter #{str}: "; password = STDIN.noecho(&:gets); end #inputsToUser
 
-
-
-def establish_Origin_repo(folder, account)
-#Dir.chdir("my_repositories/#{master_repo_dir}") do |x|
-		puts `git remote rm origin`
- 		#command hidden to hide credentials
- 		`git remote add origin https://#{account[:user]}:#{account[:pass]}@github.com/#{account[:user]}/#{folder.split('/')[-1]}.git` 		
-# 	 	`git remote add origin https://#{junk_account[:user]}:#{junk_account[:pass]}@github.com/#{junk_account[:user]}/#{folder.split('/')[-1]}.git`
-end
-#create subFolder as a Repo
-#1_deliverable... is create repo on Github1
-def create_Repo_From_subFolder(folder, account)
-		puts `git init`;puts `git add *`;puts `git commit -m "Initial Commit"`;
-
- 		# creates empty repo using name of given folder as repo name.
- 		# folder name is collected by spliting "folder" and string after last "/"
- 		puts `curl -u "#{account[:user]}:#{account[:pass]}" https://api.github.com/user/repos -d '{ "name": "#{folder.split('/')[-1]}" }'`
-		establish_Origin_repo(folder, account)
-		puts `git push origin master`
-end
-def checkM(main_github, main_pass) #master account
-	#if(
-	master = {user: main_github.gsub("\n", ""), pass: main_pass.gsub("\n", "")} 
-		#)puts "incorrectMasterAccount or MasterAccount credentials"
-		#masterAccountUserName()
-	 	#masterAccountpassword()
-		#checkM()
-	#end
-end
-def checkJ(secondary_github,secondary_pass)#junk account
-	#if(
-	junk = {user: secondary_github.gsub("\n", ""), pass: secondary_pass.gsub("\n", "")}
-		#)puts "incorrectMasterAccount or MasterAccount credentials"
-		#junkAccountUserName()
-		#junkAccountpassword()
-		#checkJ()
-	#end 
-end
 def inputsToUser()#parameters added would be void... hope is pass by ref.(master, junk)
 	main_github 		= accountName("Github account name for master repository")
 	secondary_github 	= accountName("Github account name for junk repositories")
@@ -105,6 +67,32 @@ def inputsToUser()#parameters added would be void... hope is pass by ref.(master
 	object = {j: junk, m: master}
 end
 
+def checkM(main_github, main_pass) #master account
+	#if(
+	master = {user: main_github.gsub("\n", ""), pass: main_pass.gsub("\n", "")} 
+		#)puts "incorrectMasterAccount or MasterAccount credentials";
+		#masterAccountUserName();masterAccountpassword();checkM()#end
+end
+def checkJ(secondary_github,secondary_pass)#junk account
+	#if(
+	junk = {user: secondary_github.gsub("\n", ""), pass: secondary_pass.gsub("\n", "")}
+		#)puts "incorrectMasterAccount or MasterAccount credentials";
+		#junkAccountUserName();junkAccountpassword();checkJ()
+	#end 
+end
+def establish_Origin_repo(folder, account)
+		#do not puts anything that shows credentials
+		puts `git remote rm origin`
+ 		`git remote add origin https://#{account[:user]}:#{account[:pass]}@github.com/#{account[:user]}/#{folder.split('/')[-1]}.git`
+end
+def create_Repo_From_subFolder(folder, account)
+		puts `git init`;puts `git add *`;puts `git commit -m "Initial Commit"`;
+ 		# creates empty repo using name of given folder as repo name.# folder name is collected by spliting "folder" and string after last "/"
+ 		puts `curl -u "#{account[:user]}:#{account[:pass]}" https://api.github.com/user/repos -d '{ "name": "#{folder.split('/')[-1]}" }'`
+		establish_Origin_repo(folder, account)
+		puts `git push origin master`
+end
+
 #task used to check validity of Github credentials.
 task :check_credentials do
 	collect_account_credentials('master')
@@ -112,31 +100,23 @@ task :check_credentials do
 	check(@master, 'master')
 	check(@junk, 'junk')
 end
-
 #task created for testing purposes to show deleting of repo.
 task :check_delete_repo do
 	folder = folderName()
 	object = inputsToUser()
-######username = accountName("username")
-######password = accountPassword("password")
 	folder = folder.gsub("\n", "")
-	#puts "#{object[:m][:user]}#{object[:m][:pass]}#{object[:j][:user]}#{object[:j][:pass]}"
 	username = object[:m][:user]
 	password = object[:m][:pass]#these two commands are for preliminary test purposes for this particular task.
-######username = username.gsub("\n", "")
-######password = password.gsub("\n", "")
 	Dir.chdir("my_repositories/#{folder}") do |x|
 		create_Repo_From_subFolder(folder, object[:m])
 		`curl -u #{username}:#{password} -X DELETE  https://api.github.com/repos/{#{username}}/{#{folder}}`
-		#`git remote rm origin`##establish_Origin_repo is now handling this section.
 	end
 end
 def doStuff(environmentFolder, folder, master, junk)
-	master_repo_dir = folder.gsub("\n", "")#master_repo_dir = folder.gsub("\n", "")
-	#Dir.chdir("#{environmentFolder}/") do |x|
-	#	puts `cp #{master_repo_dir} backup_#{master_repo_dir}` # Copy never to be touched till end
-	#end
-	#Dir.mkdir("#{environmentFolder}/submodule_builder")
+	master_repo_dir = folder.gsub("\n", "")
+	Dir.chdir("#{environmentFolder}/") do |x|
+		puts `cp -r #{master_repo_dir} backup_#{master_repo_dir}` # Copy never to be touched till end
+	end
 	Dir.chdir("#{environmentFolder}/#{master_repo_dir}") do |x|
 		#puts `git remote rm origin`
  		puts `git remote add origin https://#{master[:user]}:#{master[:pass]}@github.com/#{master[:user]}/#{x.split('/')[-1]}.git`
@@ -145,9 +125,7 @@ def doStuff(environmentFolder, folder, master, junk)
 		if(File.directory?("#{environmentFolder}/#{master_repo_dir}/#{x}"))
 			# Refactor possible
 			if !(x == ".." || x == "." || x == ".git")
-				 #puts `mv #{environmentFolder}/#{master_repo_dir}/#{x} #{environmentFolder}/#{x}`#submodule_builder/#{x}`
-				 initialize_submodule("#{environmentFolder}/#{master_repo_dir}/#{x}", junk) #submodule_builder/#{x}", junk)
-				 #if fails, check master credentials
+				 initialize_submodule("#{environmentFolder}/#{master_repo_dir}/#{x}", junk)
 				 Dir.chdir("#{environmentFolder}/#{master_repo_dir}") do |i|
 				 	puts `git rm --cached -rf #{x}`
 				 	puts `git submodule add https://github.com/#{junk[:user]}/#{x}`
@@ -169,9 +147,6 @@ def doStuff(environmentFolder, folder, master, junk)
 	end
 end
 task :submodulize_folder do
-#	object = inputsToUser()
-#	puts "#{object['junk']}"
-#	puts "#{object['master[:user]']}"
 	folder = folderName()
 	object = inputsToUser()
 	doStuff('my_repositories',folder, object[:m], object[:j])
@@ -183,16 +158,16 @@ def initialize_submodule(folder, junk_account)
 		puts `git init`
 		puts `git add *`
  		puts `git commit -m "Initial Commit"`
- 		#if fails to create empty repo, check junk credentials
+ 		#when are  checks are complete ... delete  this.#if fails to create empty repo, check junk credentials
  		while !setup_remote_repo(junk_account, folder.split('/')[-1]) do
  			junk_account = recollect_github_credentials(junk_account, 'junk')
  		end
- 		#puts `curl -u "#{junk_account[:user]}:#{junk_account[:pass]}" https://api.github.com/user/repos -d '{ "name": "#{folder.split('/')[-1]}" }'`
  		puts `git remote rm origin`
- 		#command hidden to hide credentials
+ 		#puts not used, so command hidden to hide credentials
  		 `git remote add origin https://#{junk_account[:user]}:#{junk_account[:pass]}@github.com/#{junk_account[:user]}/#{folder.split('/')[-1]}.git`
  		puts `git push origin master`
 	end
+	#this should be a method... above in this method needs refactor first.
 	#This will fail if there are no files or folders
 	#If folder contains nothing, touch a file to it
 	if Dir["#{folder}/*"].empty?
@@ -223,27 +198,21 @@ def initialize_submodule(folder, junk_account)
 	end
 end
 def setup_remote_repo(account, name)
-	# creates empty repo using name of given folder as repo name.
- 		# folder name is collected by spliting "folder" and string after last "/"
+	# posts an empty repo to github.
 	puts `curl -u "#{account[:user]}:#{account[:pass]}" https://api.github.com/user/repos -d '{ "name": "#{name}" }'`
 	repo_check = JSON.parse(`curl https://api.github.com/repos/#{account[:user]}/#{name}`)
 	return repo_check["message"].nil?
 end
 def recollect_github_credentials(account, type)
 	puts "Account credentials for #{account[:user]} (#{type} account) invalid."
-	puts "Username: "
-	username = STDIN.gets
-	puts "Password: "
-	password = STDIN.noecho(&:gets)
+	puts "Username: ";username = STDIN.gets
+	puts "Password: ";password = STDIN.noecho(&:gets)
 	new_account = {user: username.gsub("\n", ""), pass: password.gsub("\n", "")}
 	return new_account
 end
 def collect_account_credentials(type)
-	puts "Username: "
-	username = STDIN.gets
-	puts "Password: "
-	password = STDIN.noecho(&:gets)
-
+	puts "Username: ";username = STDIN.gets;
+	puts "Password: ";password = STDIN.noecho(&:gets);
 	new_account = {user: username.gsub("\n", ""), pass: password.gsub("\n", "")}
 	if type == 'master'
 		@master = new_account
@@ -255,7 +224,6 @@ def check(credentials, type)
 	if !credentials.nil?
 		response = `curl -i https://api.github.com -u #{credentials[:user]}:#{credentials[:pass]}`
 		response = JSON.parse(response[response.index('{')..-1])
-
 		if response["message"]
 			puts "Incorrect #{type} account or credentials"
 			check(collect_account_credentials(type), type)
