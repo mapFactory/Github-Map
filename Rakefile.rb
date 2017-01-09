ad8aee83c28c69110c67a56f36e001b3d85f6f12
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-01-08 21:59:55 -0500

    update Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,10 +1,6 @@
 require 'io/console'
 require 'json'
 require_relative 'methods.rb'
-task :test_inputs do
-	object = inputsToUser
-	puts object
-end
 #Live tasks
 task :check_delete_repo do #task created for testing purposes to show deleting of repo.
 	folder = folderName()
