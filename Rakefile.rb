ff204264aff4900999daaba9eaf617d9abd74866
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-01-11 00:25:27 -0500

    added readability fix and logic to test_delete_all

--- Rakefile.rb
+++ Rakefile.rb
@@ -26,15 +26,15 @@ task :Test_printInputs do object = inputsToUser();puts "#{object[:m][:user]}#{ob
 task :test_submodulize_folder do
     folder1 = "1_test_CheckReadmeAndSubdirs"#folder1; #folder1				= "new_folder";#folder1				= "2_test_MasterReponoSub"; #folder1				= "e_test_NoReadme"
     #object = inputsToUser("miketestgit02", "miketestgit02", "qzfreetf59im", "qzfreetf59im")
-    object = inputsToUser({user: "miketestgit02", pass: "qzfreetf59im"}, {user: "miketestgit02", pass: "qzfreetf59im"})
+    object = inputsToUser(master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
     Backup('Testing', folder1)
     folder_count = initialize_submodule("Testing/#{folder1}", object, 0, 'junk')#doStuff('Testing', folder1, object[:m], object[:j])
     if (folder_count == 1) then puts "No subfolders found in this repository. No actions were taken." end
 end
 task :test_delete_all do
 	folder1	= "1_test_CheckReadmeAndSubdirs";folder2 = "2_test_MasterReponoSub";folder3	= "e_test_NoReadme";	#folder1 = "new_folder"
-	object = inputsToUser("miketestgit02", "miketestgit02", "qzfreetf59im", "qzfreetf59im")
-	folder_count = initialize_submodule("Testing/#{folder1}", object, 1, 'junk')
+	object = inputsToUser(master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
+    folder_count = initialize_submodule("Testing/#{folder1}", object, 1, 'junk')
     if (folder_count == 1) then puts "No subfolders found in this repository. No actions were taken."
     else Delete_Backup('Testing', folder1) end
 end
