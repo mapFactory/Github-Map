fb816cd6c13a203bae3870a134f87a8aae628568
Ryan Murphy <murjax@gmail.com>
2017-01-22 02:27:10 +0000

    Added new credential param to handle RSpec test; Fixed two tests: confirming existance of remote repo and checking invalid credentials

--- methods.rb
+++ methods.rb
@@ -1,16 +1,21 @@
 def folderName(folder = nil) puts "Please enter folder name: "; folder = STDIN.gets; folder = folder.gsub("\n", ""); end #called specifically in task
 def accountName(str) puts "Please enter Github account name for #{str} repository: "; username = STDIN.gets; username.gsub("\n", ""); end #inputsToUser
 def accountPassword(str) puts "Please enter password for #{str} Github account: "; password = STDIN.noecho(&:gets); password.gsub("\n", ""); end #inputsToUser
-def recollect_github_credentials(account, type)
+def recollect_github_credentials(account, type, new_credentials = nil)
 	puts "Account credentials for #{account[:user]} (#{type} account) invalid."
-	puts "Username: ";username = STDIN.gets
-	puts "Password: ";password = STDIN.noecho(&:gets)
+	if(new_credentials.nil?)
+		puts "Username: ";username = STDIN.gets
+		puts "Password: ";password = STDIN.noecho(&:gets)
+	else
+		username = new_credentials[:user]
+		password = new_credentials[:pass]
+	end
 	return {user: username.gsub("\n", ""), pass: password.gsub("\n", "")}
 end
-def check(credentials, type)
+def check(credentials, type, new_credentials = nil)
 	response = `curl -i https://api.github.com -u #{credentials[:user]}:#{credentials[:pass]}`
 	response = JSON.parse(response[response.index('{')..-1])
-	response["message"] ? check(recollect_github_credentials(credentials, type), type) : credentials
+	response["message"] ? check(recollect_github_credentials(credentials, type, new_credentials), type) : credentials
 end
 def check_remote_exists(account, folder)
 	#account = (type == "master" ? object[:m] : object[:j])
