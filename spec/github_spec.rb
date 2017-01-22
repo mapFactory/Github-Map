a8f021c64bce85f527c10812b34d23c2af0e8241
Ryan Murphy <murjax@gmail.com>
2017-01-22 02:10:18 +0000

    Add tests to github_spec; Added credential param to handle RSpec test;
    squashed commit 1;

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -1,5 +1,38 @@
 require "rspec"
+require 'json'
 require_relative "../methods.rb"
 describe "the interactions with the Github API" do
+	let(:check_result) { check({user: "miketestgit02", pass: "qzfreetf59im"}, 'master') }
+	let(:check_invalid_result) { check({user: "miketestgit02", pass: "qzfreetf59i"}, 'master', {user: "miketestgit02", pass: "qzfreetf59im"}) }
+
+	let(:real_repo) { check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, 'testing') }
+	let(:fake_repo) { check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, 'foobar') }
+	it "check should confirm valid credentials" do
+		expect(check_result).to eq({user: "miketestgit02", pass: "qzfreetf59im"})
+	end
+
 	
+	it "check request valid credentials if credentials passed are invalid" do
+		expect(check_invalid_result).to eq({user: "miketestgit02", pass: "qzfreetf59im"})
+	end
+
+	it "should confirm existance of remote repo" do
+		expect(real_repo).to eq(true)
+	end
+
+	it "should confirm nonexistance of remote repo" do
+		expect(fake_repo).to eq(false)
+	end	
+
+	it "should confirm repo has been created" do
+		`curl -u "miketestgit02:qzfreetf59im" https://api.github.com/user/repos -d '{ "name": "rspec_test_repo" }' /dev/null`
+		expect( check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, "rspec_test_repo") ).to eq(true)
+	end
+
+	it "should confirm repo has been deleted" do
+		`curl -u "miketestgit02:qzfreetf59im" https://api.github.com/user/repos -d '{ "name": "rspec_test_repo" }' /dev/null`
+		delete_online_repo("rspec_test_repo", {user: "miketestgit02", pass: "qzfreetf59im"})
+		expect( check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, "rspec_test_repo") ).to eq(false)
+	end
+
 end
