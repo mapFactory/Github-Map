0a47423214af2638cb17ce9ffc50ec14514eaba9
Ryan Murphy <murjax@gmail.com>
2017-01-22 02:27:10 +0000

    Added new credential param to handle RSpec test; Fixed two tests: confirming existance of remote repo and checking invalid credentials

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -3,7 +3,7 @@ require 'json'
 require_relative "../methods.rb"
 describe "the interactions with the Github API" do
 	let(:check_result) { check({user: "miketestgit02", pass: "qzfreetf59im"}, 'master') }
-	let(:check_invalid_result) { check({user: "miketestgit02", pass: "qzfreetf59i"}, 'master') }
+	let(:check_invalid_result) { check({user: "miketestgit02", pass: "qzfreetf59i"}, 'master', {user: "miketestgit02", pass: "qzfreetf59im"}) }
 
 	let(:real_repo) { check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, 'testing') }
 	let(:fake_repo) { check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, 'foobar') }
@@ -11,10 +11,10 @@ describe "the interactions with the Github API" do
 		expect(check_result).to eq({user: "miketestgit02", pass: "qzfreetf59im"})
 	end
 
-	#fix later
-	# it "check request valid credentials if credentials passed are invalid" do
-	# 	expect(check_invalid_result).to eq({user: "miketestgit02", pass: "qzfreetf59im"})
-	# end
+	
+	it "check request valid credentials if credentials passed are invalid" do
+		expect(check_invalid_result).to eq({user: "miketestgit02", pass: "qzfreetf59im"})
+	end
 
 	it "should confirm existance of remote repo" do
 		expect(real_repo).to eq(true)
@@ -24,10 +24,10 @@ describe "the interactions with the Github API" do
 		expect(fake_repo).to eq(false)
 	end	
 
-	#fix later
-	# it "should confirm repo has been created" do
-
-	# end
+	it "should confirm repo has been created" do
+		`curl -u "miketestgit02:qzfreetf59im" https://api.github.com/user/repos -d '{ "name": "rspec_test_repo" }' /dev/null`
+		expect( check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, "rspec_test_repo") ).to eq(true)
+	end
 
 	it "should confirm repo has been deleted" do
 		`curl -u "miketestgit02:qzfreetf59im" https://api.github.com/user/repos -d '{ "name": "rspec_test_repo" }' /dev/null`
