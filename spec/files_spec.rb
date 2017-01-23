83252c5b799ab22df823e0427e5047ceccf3cdf7
Ryan Murphy <murjax@gmail.com>
2017-01-23 03:05:44 +0000

    Add folder backup tests to files_spec

--- spec/files_spec.rb
+++ spec/files_spec.rb
@@ -1,5 +1,26 @@
 require "rspec"
 require_relative "../methods.rb"
 describe "the interactions with files" do
-	
+	it "should create a backup of specified submodule folders" do
+		submodule_backup("Testing", "1_test_CheckReadmeAndSubdirs")
+		Dir.chdir("Testing") do
+			expect(File.directory?('submodulebackup_1_test_CheckReadmeAndSubdirs')).to eq(true)
+		end
+		
+	end
+
+	it "should create a backup of specified folders" do
+		Backup("Testing", "1_test_CheckReadmeAndSubdirs")
+		Dir.chdir("Testing") do
+			expect(File.directory?('backup_1_test_CheckReadmeAndSubdirs')).to eq(true)
+		end
+	end
+
+	it "should delete backup of specified folders" do
+		Backup("Testing", "1_test_CheckReadmeAndSubdirs")
+		Delete_Backup("Testing", "1_test_CheckReadmeAndSubdirs")
+		Dir.chdir("Testing") do
+			expect(File.directory?('backup_1_test_CheckReadmeAndSubdirs')).to eq(false)
+		end
+	end
 end
