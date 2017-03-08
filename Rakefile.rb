c882071298707bff7c1f265668967cfb63f0d9e0
MichaelDimmitt <MichaelGDimmitt@gmail.com>
2017-03-08 17:37:24 -0500

    changed spacing to two space standard; methods.rb and Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,45 +4,45 @@ require_relative 'methods.rb'
 #Live tasks
 task :Test_printInputs do object = inputsToUser();puts "#{object[:m][:user]}#{object[:m][:pass]}#{object[:j][:user]}#{object[:j][:pass]}"; end
 task :check_delete_repo do #task created for testing purposes to show deleting of repo.
-	folder = folderName()
-	object = inputsToUser()#check is inside inputs()
-	folder = folder.gsub("\n", "")#should this be inside folderName()?
-	Dir.chdir("my_repositories/#{folder}") do |x|
-		create_Repo_From_subFolder(folder, object[:m])
-		`curl -u #{object[:m][:user]}:#{object[:m][:pass]} -X DELETE  https://api.github.com/repos/{#{object[:m][:user]}}/{#{folder}}`
-	end
+  folder = folderName()
+  object = inputsToUser()#check is inside inputs()
+  folder = folder.gsub("\n", "")#should this be inside folderName()?
+  Dir.chdir("my_repositories/#{folder}") do |x|
+    create_Repo_From_subFolder(folder, object[:m])
+    `curl -u #{object[:m][:user]}:#{object[:m][:pass]} -X DELETE  https://api.github.com/repos/{#{object[:m][:user]}}/{#{folder}}`
+  end
 end
 task :test_check_delete_repo do
-	folder = folderName()
-	object = inputsToUser("miketestgit02", "miketestgit02", "qzfreetf59im", "qzfreetf59im")#check is inside inputs()
-	folder = folder.gsub("\n", "")
-	username = object[:m][:user]
-	password = object[:m][:pass]#these two commands are for preliminary test purposes for this particular task.
-	Dir.chdir("my_repositories/#{folder}") do |x|
-		create_Repo_From_subFolder(folder, object[:m])
-		`curl -u #{username}:#{password} -X DELETE  https://api.github.com/repos/{#{username}}/{#{folder}}`
-	end
+  folder = folderName()
+  object = inputsToUser("miketestgit02", "miketestgit02", "qzfreetf59im", "qzfreetf59im")#check is inside inputs()
+  folder = folder.gsub("\n", "")
+  username = object[:m][:user]
+  password = object[:m][:pass]#these two commands are for preliminary test purposes for this particular task.
+  Dir.chdir("my_repositories/#{folder}") do |x|
+    create_Repo_From_subFolder(folder, object[:m])
+    `curl -u #{username}:#{password} -X DELETE  https://api.github.com/repos/{#{username}}/{#{folder}}`
+  end
 end
 task :update_submodule_backup do
-	folder = folderName
-	submodule_backup("my_repositories", folder)
+  folder = folderName
+  submodule_backup("my_repositories", folder)
 end
 task :submodulize_folder do
-	object = inputsToUser()
-	automate("my_repositories", object, exist = true, type = 'master')
+  object = inputsToUser()
+  automate("my_repositories", object, exist = true, type = 'master')
 end
 task :desubmodulize_folder do
-	object = inputsToUser()
-	automate("my_repositories", object, exist = false, type= 'master')
+  object = inputsToUser()
+  automate("my_repositories", object, exist = false, type= 'master')
 end
 #test Tasks
 task :test_submodulize_folder do
-    #folder1 = "1_test_CheckReadmeAndSubdirs"#folder1; #folder1				= "new_folder";#folder1				= "2_test_MasterReponoSub"; #folder1				= "e_test_NoReadme"
-    #object = inputsToUser("miketestgit02", "miketestgit02", "qzfreetf59im", "qzfreetf59im")
-    object = inputsToUser("1_test_CheckReadmeAndSubdirs" ,master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
-    automate("Testing",object, exist = true, type= 'junk')
+  #folder1 = "1_test_CheckReadmeAndSubdirs"#folder1; #folder1				= "new_folder";#folder1				= "2_test_MasterReponoSub"; #folder1				= "e_test_NoReadme"
+  #object = inputsToUser("miketestgit02", "miketestgit02", "qzfreetf59im", "qzfreetf59im")
+  object = inputsToUser("1_test_CheckReadmeAndSubdirs" ,master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
+  automate("Testing",object, exist = true, type= 'junk')
 end
 task :test_desubmodulize_folder do
-	object = inputsToUser("1_test_CheckReadmeAndSubdirs", master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
-    automate("Testing",object, exist = false, type= 'junk')
+  object = inputsToUser("1_test_CheckReadmeAndSubdirs", master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
+  automate("Testing",object, exist = false, type= 'junk')
 end
--- methods.rb
+++ methods.rb
@@ -2,185 +2,191 @@ def folderName(folder = nil) puts "Please enter folder name: "; folder = STDIN.g
 def accountName(str) puts "Please enter Github account name for #{str} repository: "; username = STDIN.gets; username.gsub("\n", ""); end #inputsToUser
 def accountPassword(str) puts "Please enter password for #{str} Github account: "; password = STDIN.noecho(&:gets); password.gsub("\n", ""); end #inputsToUser
 def recollect_github_credentials(account, type, new_credentials = nil)
-	puts "Account credentials for #{account[:user]} (#{type} account) invalid."
-	if(new_credentials.nil?)
-		puts "Username: ";username = STDIN.gets
-		puts "Password: ";password = STDIN.noecho(&:gets)
-	else
-		username = new_credentials[:user]
-		password = new_credentials[:pass]
-	end
-	return {user: username.gsub("\n", ""), pass: password.gsub("\n", "")}
+  puts "Account credentials for #{account[:user]} (#{type} account) invalid."
+  if(new_credentials.nil?)
+    puts "Username: ";username = STDIN.gets
+    puts "Password: ";password = STDIN.noecho(&:gets)
+  else
+    username = new_credentials[:user]
+    password = new_credentials[:pass]
+  end
+  return {user: username.gsub("\n", ""), pass: password.gsub("\n", "")}
 end
 def check(credentials, type, new_credentials = nil)
-	response = `curl -i https://api.github.com -u #{credentials[:user]}:#{credentials[:pass]}`
-	response = JSON.parse(response[response.index('{')..-1])
-	response["message"] ? check(recollect_github_credentials(credentials, type, new_credentials), type) : credentials
+  response = `curl -i https://api.github.com -u #{credentials[:user]}:#{credentials[:pass]}`
+  response = JSON.parse(response[response.index('{')..-1])
+  response["message"] ? check(recollect_github_credentials(credentials, type, new_credentials), type) : credentials
 end
 def check_remote_exists(account, folder)
-	#account = (type == "master" ? object[:m] : object[:j])
-	response = `curl -i https://api.github.com/repos/#{account[:user]}/#{folder}`
-	response = JSON.parse(response[response.index('{')..-1])
-	response["message"].nil?
+  #account = (type == "master" ? object[:m] : object[:j])
+  response = `curl -i https://api.github.com/repos/#{account[:user]}/#{folder}`
+  response = JSON.parse(response[response.index('{')..-1])
+  response["message"].nil?
 end
 def inputsToUser(folder = nil, master = nil, junk = nil)
-	if folder.nil? && master.nil? && junk.nil?
-		folder = folderName()
-		master = {user: accountName("master"), pass: accountPassword("master")}
-		junk = {user: accountName("junk"), pass: accountPassword("junk")}
-	end
-	return {f: folder, j: check(junk, 'junk'), m: check(master, 'master')}
+  if folder.nil? && master.nil? && junk.nil?
+    folder = folderName()
+    master = {user: accountName("master"), pass: accountPassword("master")}
+    junk = {user: accountName("junk"), pass: accountPassword("junk")}
+  end
+  return {f: folder, j: check(junk, 'junk'), m: check(master, 'master')}
 end#parameters added would be void... hope is pass by ref.(master, junk)
 def establish_Origin_repo(folder, account)
-		puts `git remote rm origin`
- 		`git remote add origin https://#{account[:user]}:#{account[:pass]}@github.com/#{account[:user]}/#{folder.split('/')[-1]}.git`
+  puts `git remote rm origin`
+  `git remote add origin https://#{account[:user]}:#{account[:pass]}@github.com/#{account[:user]}/#{folder.split('/')[-1]}.git`
 end #do not puts anything that shows credentials		
 def create_Repo_From_subFolder(folder, account)
-		puts `git init`;puts `git add *`;puts `git commit -m "Initial Commit"`;
- 		`curl -u "#{account[:user]}:#{account[:pass]}" https://api.github.com/user/repos -d '{ "name": "#{folder.split('/')[-1]}" }' /dev/null`
-		if check_remote_exists(account, folder.split('/')[-1])
-			establish_Origin_repo(folder, account)
-			`git push origin master --quiet`
-		else
-			puts "Failed to create remote repo for #{folder}."
-		end
-		#check_remote_exist() *check remote exists here* warning: adds additional ping
-		#puts folder
-		
+  puts `git init`;puts `git add *`;puts `git commit -m "Initial Commit"`;
+  `curl -u "#{account[:user]}:#{account[:pass]}" https://api.github.com/user/repos -d '{ "name": "#{folder.split('/')[-1]}" }' /dev/null`
+  if check_remote_exists(account, folder.split('/')[-1])
+    establish_Origin_repo(folder, account)
+    `git push origin master --quiet`
+  else
+    puts "Failed to create remote repo for #{folder}."
+  end
+  #check_remote_exist() *check remote exists here* warning: adds additional ping
+  #puts folder		
 end# creates empty repo using name of given folder as repo name.# folder name is collected by spliting "folder" and string after last "/"
 def commit_andPush(x)
-	puts `git rm --cached -rf #{x}`
-	puts `git add *`;`git commit -m "Add submodule folder #{x}"`;`git push origin master --quiet`
+  puts `git rm --cached -rf #{x}`
+  puts `git add *`;`git commit -m "Add submodule folder #{x}"`;`git push origin master --quiet`
 end
 def removeFiles_addSubmodule(x, junk)
-	puts `git rm --cached -rf #{x}`
-	puts `git submodule add https://github.com/#{junk[:user]}/#{x}`
+  puts `git rm --cached -rf #{x}`
+  puts `git submodule add https://github.com/#{junk[:user]}/#{x}`
 end
 def touchwithReadme(folder)
-	if Dir["#{folder}/*"].empty?
-		puts `touch README.md`
-	end
+  if Dir["#{folder}/*"].empty?
+    puts `touch README.md`
+  end
 end
 def submodule_backup(environmentFolder, folder)
-	Dir.chdir("#{environmentFolder}/") do |x|
-    	if File.directory?("submodulebackup_#{folder}")
-    		puts `rm -rf submodulebackup_#{folder}`
-    	end
-    	puts `cp -r #{folder} submodulebackup_#{folder}` 
+  Dir.chdir("#{environmentFolder}/") do |x|
+    if File.directory?("submodulebackup_#{folder}")
+      puts `rm -rf submodulebackup_#{folder}`
     end
+    puts `cp -r #{folder} submodulebackup_#{folder}` 
+  end
 end
 def Backup(environmentFolder, folder)
-    Dir.chdir("#{environmentFolder}/") do |x|
-    	if !File.directory?("backup_#{folder}")
-    		puts `cp -r #{folder} backup_#{folder}` 
-    	end
+  Dir.chdir("#{environmentFolder}/") do |x|
+    if !File.directory?("backup_#{folder}")
+      puts `cp -r #{folder} backup_#{folder}` 
     end
+  end
 end# Copy never to be touched till end... if copy already exists it should be ignored or copy itself not be overridden
 def Delete_Backup(environmentFolder, folder)
-    Dir.chdir("#{environmentFolder}/") do |x|
-        puts `rm -rf backup_#{folder}` 
-    end
+  Dir.chdir("#{environmentFolder}/") do |x|
+    puts `rm -rf backup_#{folder}` 
+  end
 end
 def delete_online_repo(folder, account)
-	username = account[:user];password = account[:pass];
-	`curl -u #{username}:#{password} -X DELETE  https://api.github.com/repos/{#{username}}/{#{folder.split('/')[-1]}}`;#puts folder.split('/')[-1];
+  username = account[:user];password = account[:pass];
+  `curl -u #{username}:#{password} -X DELETE  https://api.github.com/repos/{#{username}}/{#{folder.split('/')[-1]}}`;#puts folder.split('/')[-1];
 end
 def surface_folder_level(folder, account, exist)
-	Dir.chdir("#{folder}") do |i| #current_directory()
-		if exist
-			touchwithReadme(folder)
-			create_Repo_From_subFolder(folder, account)
-		else
-			delete_online_repo(folder, account)
-		end
-	end
+  Dir.chdir("#{folder}") do |i| #current_directory()
+    if exist
+      touchwithReadme(folder)
+      create_Repo_From_subFolder(folder, account)
+    else
+      delete_online_repo(folder, account)
+    end
+  end
 end
 def sub_folder_level(folder, object, exist)
-	Dir.foreach(folder) do |x|
-		if(File.directory?("#{folder}/#{x}"))
-      			if !(x == ".." || x == "." || x == ".git") #sub_directories()
-                		initialize_submodule("#{folder}/#{x}", object, exist, 'junk')
-                		if exist
-                			Dir.chdir("#{folder}") do |i|
-                    				removeFiles_addSubmodule(x, object[:j])
-                    				commit_andPush(x)
-end	end	end 	end 	end	end
+  Dir.foreach(folder) do |x|
+    if(File.directory?("#{folder}/#{x}"))
+      if !(x == ".." || x == "." || x == ".git") #sub_directories()
+        initialize_submodule("#{folder}/#{x}", object, exist, 'junk')
+        if exist
+          Dir.chdir("#{folder}") do |i|
+            removeFiles_addSubmodule(x, object[:j])
+            commit_andPush(x)
+          end
+        end
+      end
+    end
+  end	
+end
 def master_has_subfolders_or_is_subfolder_already(folder, type)#subfolder would mean that type is "junk"
-	if type == "master"
-		Dir.foreach(folder) do |x|
-			if(File.directory?("#{folder}/#{x}"))
-	      			if !(x == ".." || x == "." || x == ".git") #sub_directories()
-	                		return 2
-		end	end	end
-	else
-		return true
-	end
-	#if the return has not happened by now it is presumably false.
+  if type == "master"
+    Dir.foreach(folder) do |x|
+      if(File.directory?("#{folder}/#{x}"))
+        if !(x == ".." || x == "." || x == ".git") #sub_directories()
+	  return 2
+        end
+      end
+    end
+  else
+    return true
+  end
+  #if the return has not happened by now it is presumably false.
 end
 def initialize_submodule(folder, object, exist, type)
-	# if exist && type == "master"
-	# 	clone_master(folder.split('/').first, object)
-	# 	Backup(folder.split('/').first, object[:f])
-	# 	set_submodulized(folder.split('/').first, folder.split('/')[-1]) 
-	# end
-	# if !exist && type == "master"
-	# 	unset_submodulized(folder.split('/').first, folder.split('/')[-1]) 
-	# end
-	if master_has_subfolders_or_is_subfolder_already(folder, type)
-		account = (type == "master" ? object[:m] : object[:j])
-		# check_repo_exist(account)if_object[j]
-		surface_folder_level(folder, account, exist)
-		sub_folder_level(folder, object, exist)
-	else puts "No subfolders found in this repository. No actions were taken."
-end	end# folder is full path to folder e.g.(github_repo_submodulizer/my_repositories/test/folder)
+  #if exist && type == "master"
+  #  clone_master(folder.split('/').first, object)
+  #  Backup(folder.split('/').first, object[:f])
+  #  set_submodulized(folder.split('/').first, folder.split('/')[-1]) 
+  # end
+  # if !exist && type == "master"
+  # unset_submodulized(folder.split('/').first, folder.split('/')[-1]) 
+  # end
+  if master_has_subfolders_or_is_subfolder_already(folder, type)
+    account = (type == "master" ? object[:m] : object[:j])
+    # check_repo_exist(account)if_object[j]
+    surface_folder_level(folder, account, exist)
+    sub_folder_level(folder, object, exist)
+  else puts "No subfolders found in this repository. No actions were taken."
+  end	
+end# folder is full path to folder e.g.(github_repo_submodulizer/my_repositories/test/folder)
+
 def check_local_directory_exists(folder, object)
-	if(!File.directory?("#{folder}/#{object[:f]}"))
-        	puts "did not find file #{object[:f]}"
-        	object[:f] = folderName(object[:f])
-			clone_master(folder, object)
-    	end
+  if(!File.directory?("#{folder}/#{object[:f]}"))
+    puts "did not find file #{object[:f]}"
+    object[:f] = folderName(object[:f])
+    clone_master(folder, object)
+  end
 end
 def clone_master(environmentFolder, object)
-	if check_remote_exists(object[:m], object[:f])#if it was online... rm folder if exists and clone it down.
-		Dir.chdir("#{environmentFolder}") do
-			puts `rm -rf #{object[:f]}`
-			puts `git clone https://github.com/#{object[:m][:user]}/#{object[:f]}`
-		end 
-	else 
-		check_local_directory_exists("#{environmentFolder}", object)
-	end
+  if check_remote_exists(object[:m], object[:f])#if it was online... rm folder if exists and clone it down.
+    Dir.chdir("#{environmentFolder}") do
+      puts `rm -rf #{object[:f]}`
+      puts `git clone https://github.com/#{object[:m][:user]}/#{object[:f]}`
+    end 
+  else 
+    check_local_directory_exists("#{environmentFolder}", object)
+  end
 end
 def set_submodulized(environmentFolder, folder)
-	Dir.chdir("#{environmentFolder}/#{folder}") do |x|
-		puts "file touched"
-		`touch .submodulized`
-	end
+  Dir.chdir("#{environmentFolder}/#{folder}") do |x|
+    puts "file touched"
+    `touch .submodulized`
+  end
 end
 def unset_submodulized(environmentFolder, folder)
-	Dir.chdir("#{environmentFolder}/#{folder}") do |x|
-		`rm .submodulized`
-	end
+  Dir.chdir("#{environmentFolder}/#{folder}") do |x|
+    `rm .submodulized`
+  end
 end
 def check_submodulized(environmentFolder, folder)
-	Dir.chdir("#{environmentFolder}/#{folder}") do |x|
-		return File.exist?('.submodulized')
-	end
+  Dir.chdir("#{environmentFolder}/#{folder}") do |x|
+    return File.exist?('.submodulized')
+  end
 end
 def automate(environmentFolder, object, exist, type)
-	#Below logic needs to be refactored
-	if (exist && !check_submodulized(environmentFolder, object[:f])) || (!exist && check_submodulized(environmentFolder, object[:f]))
-		if exist
-			clone_master(environmentFolder, object)
-			Backup(environmentFolder, object[:f])
-			set_submodulized(environmentFolder, object[:f]) 
-		else
-			unset_submodulized(environmentFolder, object[:f]) 
-		end
-
-		initialize_submodule("#{environmentFolder}/#{object[:f]}", object, exist, type)
-		submodule_backup(environmentFolder, object[:f])
-	else
-		puts "Folder is already submodulized. No actions taken."
-	end
-	
+  #Below logic needs to be refactored
+  if (exist && !check_submodulized(environmentFolder, object[:f])) || (!exist && check_submodulized(environmentFolder, object[:f]))
+    if exist
+      clone_master(environmentFolder, object)
+        Backup(environmentFolder, object[:f])
+        set_submodulized(environmentFolder, object[:f]) 
+    else
+      unset_submodulized(environmentFolder, object[:f]) 
+    end
+  initialize_submodule("#{environmentFolder}/#{object[:f]}", object, exist, type)
+    submodule_backup(environmentFolder, object[:f])
+  else
+    puts "Folder is already submodulized. No actions taken."
+  end	
 end
