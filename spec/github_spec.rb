5c5be312634aca39bb818a3e65812ab9825bd549
murjax <murjax@gmail.com>
2017-03-20 10:58:08 -0400

    Modify spec files to use new classes

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -1,12 +1,18 @@
 require "rspec"
 require 'json'
-require_relative "../methods.rb"
+require_relative "../environment.rb"
+require_relative "../repo_finder.rb"
+require_relative "../inputs.rb"
+#  ???????????.rb
 describe "the interactions with the Github API" do
-	let(:check_result) { check({user: "miketestgit02", pass: "qzfreetf59im"}, 'master') }
-	let(:check_invalid_result) { check({user: "miketestgit02", pass: "qzfreetf59i"}, 'master', {user: "miketestgit02", pass: "qzfreetf59im"}) }
+	inputs = Inputs.new
+	finder = Repo_Finder.new
+	environment = Environment.new
+	let(:check_result) { inputs.check({user: "miketestgit02", pass: "qzfreetf59im"}, 'master') }
+	let(:check_invalid_result) { inputs.check({user: "miketestgit02", pass: "qzfreetf59i"}, 'master', {user: "miketestgit02", pass: "qzfreetf59im"}) }
 
-	let(:real_repo) { check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, 'testing') }
-	let(:fake_repo) { check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, 'foobar') }
+	let(:real_repo) { finder.check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, 'testing') }
+	let(:fake_repo) { finder.check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, 'foobar') }
 	it "check should confirm valid credentials" do
 		expect(check_result).to eq({user: "miketestgit02", pass: "qzfreetf59im"})
 	end
@@ -26,13 +32,13 @@ describe "the interactions with the Github API" do
 
 	it "should confirm repo has been created" do
 		`curl -u "miketestgit02:qzfreetf59im" https://api.github.com/user/repos -d '{ "name": "rspec_test_repo" }' /dev/null`
-		expect( check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, "rspec_test_repo") ).to eq(true)
+		expect( finder.check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, "rspec_test_repo") ).to eq(true)
 	end
 
 	it "should confirm repo has been deleted" do
 		`curl -u "miketestgit02:qzfreetf59im" https://api.github.com/user/repos -d '{ "name": "rspec_test_repo" }' /dev/null`
-		delete_online_repo("rspec_test_repo", {user: "miketestgit02", pass: "qzfreetf59im"})
-		expect( check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, "rspec_test_repo") ).to eq(false)
+		environment.delete_online_repo("rspec_test_repo", {user: "miketestgit02", pass: "qzfreetf59im"})
+		expect( finder.check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, "rspec_test_repo") ).to eq(false)
 	end
 
 end
