d000154bde56097d916b077f7a8b9aa0e060f97e
murjax <murjax@gmail.com>
2017-03-22 10:24:22 -0400

    Convert indentation to two space standard

--- Rakefile.rb
+++ Rakefile.rb
@@ -43,12 +43,12 @@ task :desubmodulize_folder do
 end
 #test Tasks
 task :test_submodulize_folder do
-    #folder1 = "1_test_CheckReadmeAndSubdirs"#folder1; #folder1				= "new_folder";#folder1				= "2_test_MasterReponoSub"; #folder1				= "e_test_NoReadme"
-    #object = inputsToUser("miketestgit02", "miketestgit02", "qzfreetf59im", "qzfreetf59im")
-    object = Inputs.inputsToUser("1_test_CheckReadmeAndSubdirs" ,master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
-    Navigator.automate("Testing",object, exist = true, type= 'junk')
+	#folder1 = "1_test_CheckReadmeAndSubdirs"#folder1; #folder1				= "new_folder";#folder1				= "2_test_MasterReponoSub"; #folder1				= "e_test_NoReadme"
+	#object = inputsToUser("miketestgit02", "miketestgit02", "qzfreetf59im", "qzfreetf59im")
+	object = Inputs.inputsToUser("1_test_CheckReadmeAndSubdirs" ,master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
+	Navigator.automate("Testing",object, exist = true, type= 'junk')
 end
 task :test_desubmodulize_folder do
 	object = Inputs.inputsToUser("1_test_CheckReadmeAndSubdirs", master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
-    Navigator.automate("Testing",object, exist = false, type= 'junk')
+	Navigator.automate("Testing",object, exist = false, type= 'junk')
 end
