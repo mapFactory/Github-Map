5a6b686318cbb1e18edf591514523a83912d5fb5
murjax <murjax@gmail.com>
2017-03-22 10:24:22 -0400

    Convert indentation to two space standard

---
+++ app/src/backup.rb
@@ -0,0 +1,22 @@
+class Backups
+  def self.submodule_backup(environmentFolder, folder)
+    Dir.chdir("../#{environmentFolder}/") do |x|
+        if File.directory?("submodulebackup_#{folder}")
+          puts `rm -rf submodulebackup_#{folder}`
+        end
+        puts `cp -r #{folder} submodulebackup_#{folder}` 
+      end
+  end
+  def self.Backup(environmentFolder, folder)
+      Dir.chdir("../#{environmentFolder}/") do |x|
+        if !File.directory?("backup_#{folder}")
+          puts `cp -r #{folder} backup_#{folder}` 
+        end
+      end
+  end# Copy never to be touched till end... if copy already exists it should be ignored or copy itself not be overridden
+  def self.Delete_Backup(environmentFolder, folder)
+      Dir.chdir("../#{environmentFolder}/") do |x|
+          puts `rm -rf backup_#{folder}` 
+      end
+  end
+end
