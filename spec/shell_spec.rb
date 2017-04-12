aa70c73c30593e40cb4f103affc935e72eb5e0f5
murjax <murjax@gmail.com>
2017-04-12 10:09:34 -0400

    Add assertion in shell_spec for revert_github_map

--- spec/shell_spec.rb
+++ spec/shell_spec.rb
@@ -14,7 +14,7 @@ describe "rake commands" do
 		end
 	end
 
-	it "test map should put repo on Github" do
+	it "test map should put remove from Github" do
 		Dir.chdir("app") do 
 			object = Inputs.inputsToUser("4_test_RecursiveClone" ,master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
 			AppController.automate("Testing",object, exist = false, type= 'junk', false)
@@ -24,4 +24,17 @@ describe "rake commands" do
 		end
 	end
 	
+	it "test map should revert map to original and remove from Github" do
+		Dir.chdir("app") do 
+			object = Inputs.inputsToUser("4_test_RecursiveClone" ,master = {user: "miketestgit02", pass: "qzfreetf59im"},junk = {user: "miketestgit02", pass: "qzfreetf59im"})
+			AppController.automate("Testing",object, exist = true, type= 'junk', false)
+			AppController.automate("Testing",object, exist = false, type= 'junk', true)
+			finder = Folder_Setup.new
+			real_repo = finder.check_remote_exists({user: "miketestgit02", pass: "qzfreetf59im"}, '4_test_RecursiveClone')
+			expect(real_repo).to eq(false)
+			Dir.chdir("../Testing/4_test_RecursiveClone/1_folder") do
+				expect(File.exists?(".git")).to eq(false)
+			end
+		end
+	end
 end
