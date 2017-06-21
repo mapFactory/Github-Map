ad786b53f54460eed587bb58aa4fa2093a3d7042
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-06-21 11:40:58 -0400

    remove comments

--- app/src/app_controller.rb
+++ app/src/app_controller.rb
@@ -4,7 +4,23 @@ require_relative 'folder_setup.rb'
 require_relative 'backup.rb'
 require_relative 'make_readme.rb'
 class AppController
-# def surface_folder_level(folder, account, exist)
+  def confirm_expected_subfolders_exist(folder, environmentFolder)
+    backup_folder_count = folder_counter("../#{environmentFolder}/backup_#{folder}")
+    current_folder_count = folder_counter("../#{environmentFolder}/#{folder}")
+    current_folder_count == backup_folder_count
+  end
+  def folder_counter(folder)
+    count = 0
+    Dir.foreach(folder) do |x|
+      if !(x == ".." || x == "." || x == ".git") && File.directory?("#{folder}/#{x}")
+        count += 1
+        Dir.chdir("#{folder}") do |i|
+          count += folder_counter("#{x}")
+        end
+      end
+    end
+    count
+  end
   def surface_folder_level(folder, account, exist, environment)
     Dir.chdir("#{folder}") do |i| #current_directory()
       if exist
@@ -15,7 +31,7 @@ class AppController
       end
     end
   end
-  #def sub_folder_level(folder, object, exist)
+
   def sub_folder_level(folder, object, exist, environment)
     Dir.foreach(folder) do |x|
       if(File.directory?("#{folder}/#{x}"))
@@ -41,44 +57,32 @@ class AppController
     else
       return true
     end
-    #if the return has not happened by now it is presumably false.
-  end
-  def confirm_expected_subfolders_exist(folder, environmentFolder)
-    backup_folder_count = folder_counter("../#{environmentFolder}/backup_#{folder}")
-    current_folder_count = folder_counter("../#{environmentFolder}/#{folder}")
-    current_folder_count == backup_folder_count
-  end
-  def folder_counter(folder)
-    count = 0
-    Dir.foreach(folder) do |x|
-      if !(x == ".." || x == "." || x == ".git") && File.directory?("#{folder}/#{x}")
-        count += 1
-        Dir.chdir("#{folder}") do |i|
-          count += folder_counter("#{x}")
-        end
-      end
-    end
-    count
   end
   def initialize_submodule(folder, object, exist, type, environment)
-    # if exist && type == "master"
-    #   clone_master(folder.split('/').first, object)
-    #   Backup(folder.split('/').first, object[:f])
-    #   set_submodulized(folder.split('/').first, folder.split('/')[-1]) 
-    # end
-    # if !exist && type == "master"
-    #   unset_submodulized(folder.split('/').first, folder.split('/')[-1]) 
-    # end
     if master_has_subfolders_or_is_subfolder_already(folder, type)
       account = (type == "master" ? object[:m] : object[:j])
-      # check_repo_exist(account)if_object[j]
       surface_folder_level(folder, account, exist, environment)
       sub_folder_level(folder, object, exist, environment)
 
       puts exist ? "#{folder} added to map" : "#{folder} removed from map"
     else puts "No subfolders found in this repository. No actions were taken."
   end end# folder is full path to folder e.g.(github_repo_submodulizer/my_repositories/test/folder)
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   def self.automate(environmentFolder, object, exist, type)
     #Below logic needs to be refactored
     github_modifier = GithubModifier.new
