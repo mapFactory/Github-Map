a0b32fba17712b052b924f2061a979dda2185f4b
Ryan Murphy <murjax@gmail.com>
2017-11-14 18:56:26 -0500

    Allow backup class to find testing folder

--- app/src/backup.rb
+++ app/src/backup.rb
@@ -1,23 +1,27 @@
 class Backups
   def self.submodule_backup(environmentFolder, folder)
-    Dir.chdir("../#{environmentFolder}/") do |x|
-        if File.directory?("submodulebackup_#{folder}")
-          puts `rm -rf submodulebackup_#{folder}`
-        end
-        puts `cp -r #{folder} submodulebackup_#{folder}` 
+    Dir.chdir(Backups.directory(environmentFolder)) do |x|
+      if File.directory?("submodulebackup_#{folder}")
+        puts `rm -rf submodulebackup_#{folder}`
       end
+      puts `cp -r #{folder} submodulebackup_#{folder}`
+    end
   end
   def self.Backup(environmentFolder, folder)
-      Dir.chdir("../#{environmentFolder}/") do |x|
-        if !File.directory?("backup_#{folder}")
-          `cp -r #{folder} backup_#{folder}` 
-          puts "Backup of #{folder} created in #{environmentFolder}."
-        end
+    Dir.chdir(Backups.directory(environmentFolder)) do |x|
+      if !File.directory?("backup_#{folder}")
+        `cp -r #{folder} backup_#{folder}`
+        puts "Backup of #{folder} created in #{environmentFolder}."
       end
+    end
   end# Copy never to be touched till end... if copy already exists it should be ignored or copy itself not be overridden
   def self.Delete_Backup(environmentFolder, folder)
-      Dir.chdir("../#{environmentFolder}/") do |x|
-          puts `rm -rf backup_#{folder}` 
-      end
+    Dir.chdir(Backups.directory(environmentFolder)) do |x|
+      puts `rm -rf backup_#{folder}`
+    end
+  end
+
+  def self.directory(folder)
+    File.directory?("../#{folder}/") ? "../#{folder}/" : folder
   end
 end
